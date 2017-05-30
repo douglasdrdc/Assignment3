1f7fb9239a8453a77b1929824cb488f9cfb534e3
douglasdrdc <douglasdrdc@gmail.com>
2017-05-30 02:04:50 -0300

    inclusao do projeto no git

---
+++ Assignment3.Web/Models/Cliente.cs
@@ -0,0 +1,36 @@
+namespace Assignment3.Web.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Cliente")]
+    public partial class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Campo nome é obrigatório")]
+        [StringLength(500)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo sobrenome é obrigatório")]
+        [StringLength(500)]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Campo e-mail é obrigatório")]
+        [StringLength(500)]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo CPF/CNPJ é obrigatório")]
+        [StringLength(100)]
+        [Display(Name = "CPF/CNPJ")]
+        public string CpfCnpj { get; set; }
+
+        [Required(ErrorMessage = "Campo telefone é obrigatório")]
+        [StringLength(50)]
+        public string Telefone { get; set; }
+    }
+}
---
+++ Assignment3.Web/Models/ModeloDados.cs
@@ -0,0 +1,40 @@
+namespace Assignment3.Web.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ModeloDados : DbContext
+    {
+        public ModeloDados()
+            : base("name=ModeloDados")
+        {
+        }
+
+        public virtual DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .Property(e => e.Nome)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(e => e.Sobrenome)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(e => e.Email)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(e => e.CpfCnpj)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Cliente>()
+                .Property(e => e.Telefone)
+                .IsUnicode(false);
+        }
+    }
+}
